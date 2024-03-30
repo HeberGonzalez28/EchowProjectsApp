99b2ded4ae1b6d797aea8f368e8c14c20ea2d1f7
HeberGonzalez28 <ever.gonzalez83@gmail.com>
2024-03-29 22:30:53 -0600

    22:30 29/3/24

---
+++ app/src/main/java/com/example/echowprojectsapp/Activities/AcercaDe/ActivityAcercaDe.java
@@ -0,0 +1,16 @@
+package com.example.echowprojectsapp.Activities.AcercaDe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.echowprojectsapp.R;
+
+public class ActivityAcercaDe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+}
